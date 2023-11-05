45d7e989f997dfea116f4526f2da89b4926dd468
Ian Cahn <iancahn@live.com>
2023-11-05 10:55:12 +0200

    Connected DB + ENV setup
    Yup

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Our Mongo has connected to the DB");
+}).catch((err) => {
+    console.log(err);
+}
+);
+
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Skynet is running on port 3000");
+    console.log("Skynet might be running on port 3000");
     }
 );
