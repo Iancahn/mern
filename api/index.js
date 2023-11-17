a6efd8508d11a09618c663189311a5c54e7dbf6c
Ian Cahn <iancahn@live.com>
2023-11-18 00:26:49 +0200

    Git Retest
    Hopefully our ENV is not showing...

--- api/index.js
+++ api/index.js
@@ -2,18 +2,19 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
-    console.log("Our Mongo has connected to the DB");
+    console.log("Mongo is !wsorking");
 }).catch((err) => {
-    console.log(err);
-}
-);
-
+    console.log("The error is:" + err);
+});
 
 const app = express();
+
+app.use(express.json()); //alow JSON as input on server
 
 app.listen(3000, () => {
     console.log("Skynet might be running on port 3000");
@@ -21,3 +22,4 @@ app.listen(3000, () => {
 );
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
