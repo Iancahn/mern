d699d81530cde9eb7404ac3bd4edb0e2ead7bc4f
Ian Cahn <iancahn@live.com>
2023-12-23 16:37:57 +0200

    Added Redux Persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // import { getDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
 
+// next we combine reducers, this is abit advanced for me
+const rootReducer = combineReducers({user: userReducer})
+
+// this persisted reducer allows us to keep state/user on page refresh
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
   // set serializalbe Check will prevent some error later?
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware({
@@ -11,3 +25,4 @@ export const store = configureStore({
   }),
 });
 
+export const persistor = persistStore(store);
