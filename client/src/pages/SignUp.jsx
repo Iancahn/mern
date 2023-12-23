7570d5b3861edf74e50c32ef44b91b99b289cb47
Ian Cahn <iancahn@live.com>
2023-12-23 09:44:35 +0200

    Updated Signup functionality
    UseNavigate and better error handling

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,39 +1,51 @@
 import {useState} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 
-
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  // we are saving values from our form using state
   const handleChange =(e) =>{
       setFormData({
         ...formData,
         [e.target.id]: e.target.value,
       })
   };
+
+  // on submit, we need a function call to handle submit
+  // as we are using await in our fetch below, we use async
   const handleSubmit = async(e)=> {
+    // we prevent default ,because the default is refresh page
     e.preventDefault();
-    setLoading(true);
-    const res = await fetch('/api/auth/signup',
-    {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    if(data.success === false){
+    // below we will add a try statement to see if we can catch any errors on frontend
+    try{
+      setLoading(true);
+      // below we take our formData, we stringify it, then post it to our signup function in our auth controller,
+      // which then posts it to our MongoDB
+      const res = await fetch('/api/auth/signup',
+      {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false){
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
       setLoading(false);
-      setError(data.message);
-      return;
+      setError(null);
+      navigate('/signin');
+    } catch (error){
+      setLoading(false);
+      setError(error.message);
     }
-    setLoading(false);
-    setError(null);
-    navigate('/signin');
-    console.log(data);
   };
   console.log(formData);
 
@@ -44,7 +56,8 @@ export default function SignUp() {
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'onChange={handleChange}/>
         <input type="text" placeholder='email' className='border p-3 rounded-lg' id='email'onChange={handleChange}/>
         <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password'onChange={handleChange}/>
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">{loading ? 'Loading...' : 'Sign Up'}</button>
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? 'Loading...' : 'Sign Up'}</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+// import step here is to create a proxy, so that everytime someone goes to /api, we point them to our target
+// basically going "each time you see the /api, add http://localhost:3000" at the beginning
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
