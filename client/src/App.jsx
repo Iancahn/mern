ac17abf3796846e4b791c46399ccdb8717d3e36f
Ian Cahn <iancahn@live.com>
2023-12-26 01:00:45 +0200

    Update Header for Google
    We also made profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
+import PrivateRoute from './components/PrivateRoute';
 import Profile from './pages/Profile';
 // import Components
 import Header from './components/Header';
@@ -14,14 +15,17 @@ import Header from './components/Header';
 export default function App() {
   return (
     // we then cover everything with the browser router, routes and individual route(s)
+    // The private route makes it so that you can only see the profile page when you are logged in
     <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="/signin" element={<SignIn/>} />
-      <Route path="/signup" element={<SignUp/>} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
       <Route path="/about" element={<About/>} />
+      <Route element = {<PrivateRoute />} >
       <Route path="/profile" element={<Profile/>} />
+      </Route>
     </Routes>
     </BrowserRouter>
   )
