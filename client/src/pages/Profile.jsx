e2ffcaafa183bbe2d23cffa21dc1f89380541149
Ian Cahn <iancahn@live.com>
2023-12-26 11:27:35 +0200

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,55 @@
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import{ getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
+
+// so for profile image replacement, we create an input with a ref of fileRef. We thexn have an onClick event listener on the profile pic
+// then you can upload new image, but to ensure you can only upload images, you use accept="image/*" on your hidden input
 
 export default function Profile() {
-  const {currentUser} = useSelector((state) => state.user)
+  const {currentUser} = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  console.log(filePerc)
+  console.log(file)
+  console.log(fileUploadError);
+
+  useEffect(() => {
+    if(file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error)=>{
+        setFileUploadError(true);
+      },
+      ()=>{
+        getDownloadURL(uploadTask.snaptshot.ref).then((downloadURL) => {
+          setFormData({...formData, avatar : downloadURL});
+        })
+      })
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.avatar} alt="profile"
+        <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*"/>
+        <img src={currentUser.avatar} onClick={()=>fileRef.current.click()} alt="profile"
         className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
         <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" />
         <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" />
