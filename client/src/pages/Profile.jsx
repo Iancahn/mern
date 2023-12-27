8ce88f692a6dc3c0b2515b87a36818e0434c9249
Ian Cahn <iancahn@live.com>
2023-12-27 13:25:56 +0200

    Complete Image Upload Functionality
    Pretty interesting

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,9 +13,6 @@ export default function Profile() {
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
-  console.log(filePerc)
-  console.log(file)
-  console.log(fileUploadError);
 
   useEffect(() => {
     if(file) {
@@ -34,23 +31,34 @@ export default function Profile() {
         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setFilePerc(Math.round(progress));
       },
-      (error)=>{
+      (error) =>{
         setFileUploadError(true);
       },
-      ()=>{
-        getDownloadURL(uploadTask.snaptshot.ref).then((downloadURL) => {
-          setFormData({...formData, avatar : downloadURL});
-        })
-      })
-  }
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => 
+          setFormData({...formData, avatar : downloadURL})
+      );
+      }
+      );
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
         <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*"/>
-        <img src={currentUser.avatar} onClick={()=>fileRef.current.click()} alt="profile"
+        <img src={formData.avatar || currentUser.avatar} onClick={()=>fileRef.current.click()} alt="profile"
         className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
+        <p className="text-sm mx-auto font-bold">
+          {fileUploadError ? 
+        ( <span className="text-red-700">Error Image Upload(Image must be less than 2mb)</span>)
+         : filePerc > 0 && filePerc < 100 ?
+        ( <span className="text-slate-700">{`Uploading ${filePerc}%`} </span> )
+         : filePerc === 100 ?
+        ( <span className ="text-green-700">Image Successfully Uploaded!</span>)
+             : ("")
+          }
+        </p>
         <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" />
         <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" />
         <input type="text" placeholder="password" className="border p-3 rounded-lg" id="password" />
